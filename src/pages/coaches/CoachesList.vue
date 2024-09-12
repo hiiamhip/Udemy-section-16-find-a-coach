d3c64f4ca137b261d3c93e7364ecd1b91d37e8fd
trantuanhiep <trantuanhiep420@gmail.com>
2024-09-12 23:15:23 +0700

    http request

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button v-if="!isCoach" link to="/register"
           >Register as Coach</base-button
         >
@@ -68,10 +68,19 @@ export default {
       return this.$store.getters['coaches/hasCoaches'];
     },
   },
+  created() {
+    this.loadCoaches();
+  },
   methods: {
     setFilter(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
+    },
+    // log() {
+    //   console.log(this.$store.getters['coaches/coaches']);
+    // },
   },
 };
 </script>
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -9,6 +9,45 @@ export default {
       areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData);
+    const response = await fetch(
+      `https://udemy-find-a-coach-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error handling
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://udemy-find-a-coach-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    const coaches = [];
+
+    for (let key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
   },
 };
